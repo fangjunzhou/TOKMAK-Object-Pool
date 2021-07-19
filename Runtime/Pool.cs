5c0a8ce1d64c12a99a0ba239fbb4bce8f9464055
DESKTOP-EDENOHJ\Fangjun Zhou <fzhou48@wisc.edu>
2021-07-19 11:30:31 +0800

    Update: Pool Change the pooling mode from array to queue

--- Runtime/Pool.cs
+++ Runtime/Pool.cs
@@ -1,133 +1,212 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace kTools.Pooling
 {
-    abstract class Pool
+    internal abstract class Pool
     {
-#region Fields
-        readonly object m_Key;
-#endregion
+        #region Fields
 
-#region Constructors
-        public Pool(object key)
+        #endregion
+
+        #region Constructors
+
+        public Pool(object key, bool expandable)
         {
-            m_Key = key;
+            this.key = key;
+            this.expandable = expandable;
         }
-#endregion
 
-#region Properties
-        public object key => m_Key;
-#endregion
+        #endregion
+
+        #region Properties
+
+        public object key { get; }
+        
+        public bool expandable { get; }
+
+        #endregion
     }
 
-    sealed class Pool<T> : Pool, IDisposable
+    internal sealed class Pool<T> : Pool, IDisposable
     {
-#region Fields
-        readonly T m_Source;
-        readonly Instance<T>[] m_Instances;
-        readonly Processor<T> m_Processor;
-#endregion
+        #region Constructors
 
-#region Constructors
-        public Pool(object key, T source, int instanceCount, Processor<T> processor) : base(key)
+        public Pool(object key, T source, int instanceCount, Processor<T> processor, bool expandable) : base(key, expandable)
         {
             // Set data
-            m_Source = source;
-            m_Instances = new Instance<T>[instanceCount];
-            m_Processor = processor;
+            this.source = source;
+            activeInstances = new List<Instance<T>>();
+            instances = new Queue<Instance<T>>();
+            this.processor = processor;
 
             // Create instances
-            for(int i = 0; i < instanceCount; i++)
+            for (var i = 0; i < instanceCount; i++)
             {
                 var obj = processor.CreateInstance(key, source);
                 var instance = new Instance<T>(obj);
-                instances[i] = instance;
+                instances.Enqueue(instance);
             }
         }
-#endregion
 
-#region Properties
-        public T source => m_Source;
-        public Instance<T>[] instances => m_Instances;
-        public Processor<T> processor => m_Processor;
-#endregion
+        #endregion
 
-#region IDisposable
+        #region IDisposable
+
         public void Dispose()
         {
             // Destroy instances
-            var instanceCount = instances.Length;
-            for(int i = 0; i < instanceCount; i++)
-            {
-                processor.DestroyInstance(key, instances[i].obj);
-            }
+            var instanceCount = instances.Count;
+            for (var i = 0; i < instanceCount; i++) processor.DestroyInstance(key, instances.Dequeue().obj);
         }
-#endregion
 
-#region Instance
+        #endregion
+
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        public T source { get; }
+
+        public List<Instance<T>> activeInstances { get; }
+        
+        public Queue<Instance<T>> instances { get; }
+
+        public Processor<T> processor { get; }
+
+        #endregion
+
+        #region Instance
+
         public T GetInstance()
         {
             Instance<T> value = null;
 
-            void GetInactiveInstance()
+            // void GetInactiveInstance()
+            // {
+            //     if (value != null)
+            //         return;
+            //
+            //     foreach (var instance in instances)
+            //         if (!instance.activeSelf)
+            //             value = instance;
+            //     value = null;
+            // }
+            //
+            // void GetOldestInstance()
+            // {
+            //     if (value != null)
+            //         return;
+            //
+            //     var oldestTime = Mathf.Infinity;
+            //     var oldestIndex = 0;
+            //     for (var i = 0; i < instances.Length; i++)
+            //         if (instances[i].activeTime < oldestTime)
+            //         {
+            //             oldestTime = instances[i].activeTime;
+            //             oldestIndex = i;
+            //         }
+            //
+            //     value = instances[oldestIndex];
+            // }
+            
+            void GetInstanceFromQueue()
             {
-                if(value != null)
+                if (value != null)
+                    return;
+
+                if (instances.Count == 0)
+                {
+                    value = null;
+                    return;
+                }
+
+                value = instances.Dequeue();
+            }
+
+            void GetNewInstance()
+            {
+                if (value != null)
                     return;
                 
-                foreach(var instance in instances)
-                {
-                    if(!instance.activeSelf)
-                        value = instance;
-                }
-                value = null;
+                var obj = processor.CreateInstance(key, source);
+                value = new Instance<T>(obj);
             }
 
             void GetOldestInstance()
             {
-                if(value != null)
+                if (value != null)
                     return;
                 
+                if (activeInstances.Count == 0)
+                {
+                    throw new NullReferenceException("Both instances and activeInstances are null.");
+                }
+
                 var oldestTime = Mathf.Infinity;
                 var oldestIndex = 0;
-                for(int i = 0; i < instances.Length; i++)
-                {
-                    if(instances[i].activeTime < oldestTime)
+                for (var i = 0; i < activeInstances.Count; i++)
+                    if (activeInstances[i].activeTime < oldestTime)
                     {
-                        oldestTime = instances[i].activeTime;
+                        oldestTime = activeInstances[i].activeTime;
                         oldestIndex = i;
                     }
-                }
-                value = instances[oldestIndex];
+                
+                value = activeInstances[oldestIndex];
             }
 
             // Get instance
-            GetInactiveInstance();
-            GetOldestInstance();
-            
+            GetInstanceFromQueue();
+            if (expandable)
+            {
+                GetNewInstance();
+            }
+            else
+            {
+                GetOldestInstance();
+            }
+
             // Enable instance
             value.SetActive(true);
             processor.OnEnableInstance(key, value.obj);
+            
+            // Add the instance to active list
+            activeInstances.Add(value);
+            
             return value.obj;
         }
 
         public void ReturnInstance(T value)
         {
-            // Find instance 
-            foreach(var instance in instances)
+            // // Find instance 
+            // foreach (var instance in instances)
+            //     if (instance.obj.Equals(value))
+            //     {
+            //         // Disable instance
+            //         instance.SetActive(false);
+            //         processor.OnDisableInstance(key, instance.obj);
+            //         return;
+            //     }
+            //
+            // // Instance not tracked
+            // Debug.LogWarning($"Pool ({key}) does not contain object ({value}).");
+
+            for (int i = 0; i < activeInstances.Count; i++)
             {
-                if(instance.obj.Equals(value))
+                if (activeInstances[i].obj.Equals(value))
                 {
-                    // Disable instance
-                    instance.SetActive(false);
-                    processor.OnDisableInstance(key, instance.obj);
-                    return;
+                    activeInstances[i].SetActive(false);
+                    processor.OnDisableInstance(key, activeInstances[i].obj);
+                    activeInstances.RemoveAt(i);
                 }
             }
-
-            // Instance not tracked
-            Debug.LogWarning($"Pool ({key}) does not contain object ({value}).");
+            var instance = new Instance<T>(value);
+            instances.Enqueue(instance);
         }
-#endregion
+
+        #endregion
     }
 }
